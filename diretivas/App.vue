69ff1a35af5485c74550cb0c987a6e40e2e15fe0
eduardonk9999 <thiago.novak7@gmail.com>
2021-01-15 16:27:21 -0300

    Add, aulas iniciais

---
+++ diretivas/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Hello {{ name }}</h1>
+    <input type="text" v-model="name">
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      name: 'Eduardo'
+    })
+  }
+</script>
